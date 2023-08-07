afe707a2511b83f317bd03fe967b12ea79118b53
Manas <iamscientistmanas@gmail.com>
2023-08-08 00:17:01 +0530

    Phase-1 file uploaded

---
+++ server.py
@@ -0,0 +1,16 @@
+#TODO - 1 : explore requests, datetime,time modules in pyton and import it in this project
+
+#TODO - 2 : Look into data.csv file, it contains data about Tirupati location, we will modify it once we complete first phase of this project
+
+#TODO - 3 : To get json file content rendered in your browser in readable format download - JSON Viewer Pro extension in Chrome
+
+# http://api.open-notify.org/iss-now.json will give you the data in json format, try to open this link after downloading the aove mentioned extension
+
+#TODO - 4: Using requests module, get the data from the API by sending requeat, try to figure it out yourself
+
+#TODO - 5: Convert the data receive into json format use. To store data in json in another varible use syntax: 
+# <var name> = <data requested variable name>.json()
+
+#TODO - 6 Access and store the city's data and store it in the variable using opnen and with (explore yourself, use stackoverflow)
+
+#TODO - 7: Compare the latitude and longitude of ISS and City's one if it's n range, print its overhead, otherwise print it's current latitude and longitude
